c3a053db62aac24ab817d43ad779e7d63b191357
Sid2169 <siddharthasingh21109572@gmail.com>
2024-11-25 07:46:20 +0530

    Added solution to sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
-
-};
+const sumAll = function (num1, num2) {
+    if (!(typeof(num1) === 'number') ||
+        !(typeof(num2) === 'number') ||
+        (num1 < 0) || (num2 < 0) || 
+         Math.floor(num1) !== num1 || 
+         Math.floor(num2) !== num2) {return 'ERROR';}
+    let n = Math.abs(num1 - num2) + 1; 
+    return (n / 2) * (num1 + num2);     
+    }
 
 // Do not edit below this line
 module.exports = sumAll;
