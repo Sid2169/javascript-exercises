94e6d202851f88a4dd305d982832449edab545d5
Sid2169 <siddharthasingh21109572@gmail.com>
2024-11-26 02:29:22 +0530

    added solution to getTheTitle

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const titles = []; // Create an empty array to store the titles
+    for (let i = 0; i < books.length; i++) {
+        titles.push(books[i].title); // Add the title of each book to the titles array
+    }
+    return titles; // Return the array of titles
 };
 
 // Do not edit below this line
