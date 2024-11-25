83b6cf58f72f84dcf280ff4162652df3ade9c581
Sid2169 <siddharthasingh21109572@gmail.com>
2024-11-26 00:39:25 +0530

    added solution to calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,43 @@
-const add = function() {
+const add = function(num1, num2) {
+  return num1 + num2;
 	
-};
+}
 
-const subtract = function() {
-	
-};
+const subtract = function(num1, num2) {
+	return num1 - num2;
+}
 
-const sum = function() {
-	
-};
+const sum = function(arr) {
+	let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
 
-const multiply = function() {
+const multiply = function(arr) {
+  let product = 1;
+  for (let i = 0; i < arr.length; i++) {
+    product *= arr[i];
+  }
+  return product;
+}
 
-};
+const power = function(num, power) {
+	let result = 1;
+  for (let i = 0; i < power; i++) {
+    result *= num;
+  }
+  return result;
+}
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const factorial = function(num) {
+	let factorial = 1;
+  for (let i = 1; i <= num; i++) {
+    factorial *= i;
+  }
+  return factorial;
+}
 
 // Do not edit below this line
 module.exports = {
