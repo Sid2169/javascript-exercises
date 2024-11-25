afef181b548245418b9d45191b174d3bdb9010fc
Sid2169 <siddharthasingh21109572@gmail.com>
2024-11-26 01:35:43 +0530

    added solution to palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let compressedString = '';
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] !== ' ' && string[i] !== '.'&& string[i] !== '!' && string[i] !== ',') compressedString += string[i].toLowerCase();
+    }
 
-};
+    let reverse = '';
+
+    for (let i = compressedString.length - 1; i >= 0; i--) {
+        reverse += compressedString[i];
+    }
+
+    return (reverse === compressedString);
+}
 
 // Do not edit below this line
 module.exports = palindromes;
